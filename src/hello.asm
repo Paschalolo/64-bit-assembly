4020ab2a27af48642a0256199e387617676c22a5
Paschal <pastechnology1@gmail.com>
2025-12-08 10:57:11 -0600

    64  bit bqby

--- src/hello.asm
+++ src/hello.asm
@@ -2,6 +2,10 @@
 ; DO NOT OPEN NOT FOR USE 
 SECTION .data 
     bVal db 0;
+    bNum db 42 
+    wNum dw 4321 
+    dNum dd 420000
+    qNum dq 42000000000
 mov eax , 100 ; eax = 0X00000064
 mov rcx , -1 ; rcx = 0XFFFFFFFFFFFFFFFF
 mov ecx , eax
@@ -15,3 +19,13 @@ mov bl , al;
 movzx cx , byte[bVar];
 movzx ebx , cx ; Unsigned conversions 
 movzx rbx , cl ; 
+
+movsx eax, -byte[bVar] ; signed conversion rtot upper limit 
+movsxd rax , dword[bVal]; used to convert a double word to a quad word 
+
+; Arithmetic for integers 
+
+add rax , rbx ; addition of vairables 
+
+
+; incermentation 
