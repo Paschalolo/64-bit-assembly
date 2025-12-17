2cdd352c172239cdc322f01f7c43fc87889a76d3
Paschalolo <pastechnology1@gmail.com>
2025-12-16 20:33:59 -0600

    64  bit bqby

---
+++ src/conversion.asm
@@ -0,0 +1,34 @@
+
+; Name : Ahnmsis Pascahl 
+; Execuatbele : conversion
+; Purpose : to convert numbers to string
+
+SECTION .bss ; section to store uninitialized data 
+	FinalMessage resb 100
+SECTION .data ; section to store initiialized data 
+	SYS_exit equ 60 ; call for terminaltion 
+	EXIT_SUCCESS equ 0 ; exit succesfully 
+
+	; Data string 
+	Message db "The number you write is " 10 
+	Messagelen equ $-Message
+	
+	; Look up table for ASCII conversion 
+	ASCII_TABLE db 0X30, 0X31, 0X32 , 0X33, 0X34 , 0X35
+		    db 0X36 ,0X37, 0X38 , 0X39
+SECTION .text ; section taht contains the code
+	global _start 
+_start : 
+	mov r12 , 9; 
+
+: conversion using table look up and writing function 
+Write : 	
+	mov rax , 0X01 ; syscall for write function 
+	mov rdi , 1, 
+	mov rsi , 
+
+; termination of the program 
+Exit : 
+	mov rax , SYS_exit 
+	mov rdi , EXIT_SUCCESS
+	syscall 
